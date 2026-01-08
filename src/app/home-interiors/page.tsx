5fbef54db495836985b7a7d2a9eed9cec0320555
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-01-08 11:58:50 +0530

    added home-interior page

---
+++ src/app/home-interiors/page.tsx
@@ -0,0 +1,12 @@
+import { INTERIOR_SCENES } from "@/lib/interiors-data";
+import InteriorClientUI from "@/pages/home-Interior/InteriorClientUI";
+
+// This is a Server Component by default
+export default function HomeInteriorsPage() {
+  return (
+    <main className="bg-[#fcfcfc] min-h-screen overflow-x-hidden">
+      {/* Passing the data as a prop to the Client UI */}
+      <InteriorClientUI scenes={INTERIOR_SCENES} />
+    </main>
+  );
+}
